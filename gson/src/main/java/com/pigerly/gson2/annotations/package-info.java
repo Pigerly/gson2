d20c0d3e7736428588b39aeea63013026d6a13c6
Pigerly <contact@pigerly.com>
2020-09-16 01:35:13 -0400

    Pig's Fixups

---
+++ gson/src/main/java/com/pigerly/gson2/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.pigerly.gson2.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.pigerly.gson2.annotations;
